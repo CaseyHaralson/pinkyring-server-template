c139c776ecdfddc75162db13cca432e9f9237123
Casey Haralson <casey.haralson@gmail.com>
2022-10-26 18:59:06 -0600

    testing a call from the api package with a dependency on the core package

--- packages/public/api/src/app.ts
+++ packages/public/api/src/app.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import TestService from '@pinkyring/core/services/testService';
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('hello world!');
+});
+
+app.get('/test', (req, res) => {
+  const service = new TestService();
+  res.send('trying to test the service: ' + service.test('a message from the api'));
 });
 
 app.listen(3000, () => {
