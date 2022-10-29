3e06ac99be1ec0e33469c48b5c4ac53c130ba2b1
Casey Haralson <casey.haralson@gmail.com>
2022-10-28 21:59:36 -0600

    testing jest

---
+++ packages/tests/core/services/testService.test.ts
@@ -0,0 +1,8 @@
+import {expect, test} from '@jest/globals';
+import TestRepository from '@pinkyring/core/interfaces/testRepository';
+import TestService from '@pinkyring/core/services/testService';
+
+test('returns a test messsage', () => {
+  const s = new TestService(new Object() as TestRepository);
+  expect(s.test('test')).toBe('got the message: test');
+});
