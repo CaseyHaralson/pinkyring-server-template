707d038fa9d0ecbfa6097c277c87377d1b2bbf76
Casey Haralson <casey.haralson@gmail.com>
2022-10-29 10:57:31 -0600

    splitting jest test command into two so one runs compiled js tests and the other runs ts tests. the watch version still needs some work because the tests run twice per file change

--- packages/core/src/services/testService.ts
+++ packages/core/src/services/testService.ts
@@ -8,7 +8,7 @@ class TestService {
 
   test(message: string) {
     console.log('message from the TestService test function: ' + message);
-    return 'got the message: ' + message;
+    return 'got the message: ' + message + '2';
   }
 
   getData() {
