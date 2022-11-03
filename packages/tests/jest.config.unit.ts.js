96dbc3a0e2bd584d5f523b4c5b4c4e8a6773db09
Casey Haralson <casey.haralson@gmail.com>
2022-11-03 17:29:45 -0600

    reworking how tests work a bit. going to try adding integration tests and don't want them running all the time like unit tests

--- packages/tests/jest.config.unit.ts.js
+++ packages/tests/jest.config.unit.ts.js
@@ -2,5 +2,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ['**/tests/src/**/*.test.ts'],
   //modulePathIgnorePatterns: ['/bin/'], // this was causing the tests to run before the ts rebuild finished, so need something better here
 };
