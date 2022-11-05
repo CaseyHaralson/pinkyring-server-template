56cd9b2bcf87989859168952e5986acf88b349f5
Casey Haralson <casey.haralson@gmail.com>
2022-11-04 21:48:50 -0600

    adding another test lambda endpoint

---
+++ packages/infrastructure/lambdas/src/test/getData.ts
@@ -0,0 +1,14 @@
+import {APIGatewayProxyEvent, APIGatewayProxyResult} from 'aws-lambda';
+import container from '@pinkyring/di-container/container';
+
+const service = container.resolveTestService();
+
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const result = service.getData();
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result),
+  };
+};
