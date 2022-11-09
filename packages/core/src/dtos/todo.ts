ecab02b10d624faa7b4361806019b0ab35bf5b78
Casey Haralson <casey.haralson@gmail.com>
2022-11-08 18:07:00 -0700

    trying to add database connections and a migrate github action

--- packages/core/src/dtos/todo.ts
+++ packages/core/src/dtos/todo.ts
@@ -6,3 +6,8 @@ export interface Todo {
   createdDt?: string;
   completedDt?: string;
 }
+
+//import {Todo as test} from '@prisma/client';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+//export interface Todo extends test {}
