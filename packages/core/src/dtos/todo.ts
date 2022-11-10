84c19b6399c76bb03f16e42d10e6af660a7310dd
Casey Haralson <casey.haralson@gmail.com>
2022-11-09 20:25:44 -0700

    removed unused eslint comment to pass lint test

--- packages/core/src/dtos/todo.ts
+++ packages/core/src/dtos/todo.ts
@@ -9,5 +9,4 @@ export interface Todo {
 
 //import {Todo as test} from '@prisma/client';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
 //export interface Todo extends test {}
