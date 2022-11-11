c79e20efb6742e289edbb62f4469d65d96432c1c
Casey Haralson <casey.haralson@gmail.com>
2022-11-10 19:42:39 -0700

    asdf

---
+++ packages/infrastructure/repositories/src/util/migrate_lambda.ts
@@ -0,0 +1,20 @@
+import {execa} from 'execa';
+import * as path from 'path';
+
+// export async function handler() {
+//   // await execa('prisma', ['migrate', 'deploy'], {
+//   //   preferLocal: true,
+//   //   stdio: 'inherit',
+//   // });
+//   await execa(path.resolve(process.cwd(), './../../migrate.sh'), [
+//     process.env.DATABASE_URL as string,
+//   ]);
+// }
+
+export const handler = async () => {
+  await execa(path.resolve(process.cwd(), './../../migrate.sh'), [
+    process.env.DATABASE_URL as string,
+  ]);
+
+  return true;
+};
