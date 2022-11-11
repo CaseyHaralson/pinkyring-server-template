7cfff2edb759c24cdbd2602f3a3e0c2ddd63e17b
Casey Haralson <casey.haralson@gmail.com>
2022-11-10 19:03:57 -0700

    trying to get a lambda to migrate the db

--- packages/infrastructure/repositories/migrate.lambda.js
+++ packages/infrastructure/repositories/migrate.lambda.js
@@ -1,8 +1,12 @@
 import {execa} from 'execa';
+import * as path from 'path';
 
 export async function handler() {
-  await execa('prisma', ['migrate', 'deploy'], {
-    preferLocal: true,
-    stdio: 'inherit',
-  });
+  // await execa('prisma', ['migrate', 'deploy'], {
+  //   preferLocal: true,
+  //   stdio: 'inherit',
+  // });
+  await execa(path.resolve(process.cwd(), './migrate.sh'), [
+    process.env.DATABASE_URL,
+  ]);
 }
