bec3b2c4351107d2a8ef0728f2072bb4ce9d0e75
Casey Haralson <casey.haralson@gmail.com>
2022-11-26 09:32:37 -0700

    adding a config file reader

--- packages/infrastructure/util/src/configFileReader.ts
+++ packages/infrastructure/util/src/configFileReader.ts
@@ -1,7 +1,29 @@
 import {IConfigFileReader} from '@pinkyring/core/interfaces/IConfig';
+import find from 'find-up';
+import dotenv, {DotenvParseOutput} from 'dotenv';
+import fs from 'fs';
 
 export default class ConfigFileReader implements IConfigFileReader {
+  private _envProps;
+  constructor() {
+    const envFilePath = this.findEnvFile();
+    this._envProps = this.parseEnvFile(envFilePath);
+  }
+
   getValue(key: string): string | undefined {
-    throw new Error('Method not implemented.');
+    const value = this._envProps[key];
+    return value;
+  }
+
+  private findEnvFile() {
+    return find.findUpSync('.env');
+  }
+
+  private parseEnvFile(filePath: string | undefined) {
+    let parsed = {} as DotenvParseOutput;
+    if (filePath) {
+      parsed = dotenv.parse(fs.readFileSync(filePath, {encoding: 'utf8'}));
+    }
+    return parsed;
   }
 }
