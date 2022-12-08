e0882efa57abb5c5af2cdd42ab08b8eb8af8e546
Casey Haralson <casey.haralson@gmail.com>
2022-12-07 21:32:38 -0700

    updates

--- .env-maker.js
+++ .env-maker.js
@@ -68,10 +68,10 @@ function generatePassword() {
   // #@ (in mysql/prisma)
   const chars =
     '0123456789abcdefghijklmnopqrstuvwxyz!$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  const passwordLength = 12;
+  const passwordLength = 13;
 
   let password = '';
-  for (let i = 0; i <= passwordLength; i++) {
+  for (let i = 0; i < passwordLength; i++) {
     const randomNumber = Math.floor(Math.random() * chars.length);
     password += chars.substring(randomNumber, randomNumber + 1);
   }
