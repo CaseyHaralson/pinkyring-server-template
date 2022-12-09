f06bb8e3831a3a96ed5fe956780ae95c29edf164
Casey Haralson <casey.haralson@gmail.com>
2022-12-09 09:23:18 -0700

    trying to handle local prisma .env file a little better

--- packages/infrastructure/relationaldb/prisma/run-prisma-command.js
+++ packages/infrastructure/relationaldb/prisma/run-prisma-command.js
@@ -12,17 +12,25 @@ if (args.length < 3) {
 const argString = args.slice(2).join(' ');
 // console.log(argString);
 
-// copy the .env file from the parent to the prisma folder
-// sometimes prisma needs the local .env file, and sometimes it doesn't
-//console.log(`copying the .env file from the parent to the prisma folder`);
-shell.exec(
-  `cd ../../../../ && npx copyfiles .env packages/infrastructure/relationaldb`
-);
+const commandsThatDontNeedLocalEnv = ['db seed'];
+let makeLocalEnvFile = true;
+if (commandsThatDontNeedLocalEnv.includes(argString.toLowerCase())) {
+  makeLocalEnvFile = false;
+}
+
+if (makeLocalEnvFile) {
+  // copy the .env file from the parent to the prisma folder
+  // sometimes prisma needs the local .env file, and sometimes it doesn't
+  //console.log(`copying the .env file from the parent to the prisma folder`);
+  shell.exec(`cd ../ && npm run prisma:env`);
+}
 
 // run the prisma command
 //console.log(`running the prisma command "npx prisma ${argString}"`);
 shell.exec(`cd ../ && npx prisma ${argString}`);
 
-// delete the prisma .env file
-//console.log(`removing the prisma .env file`);
-shell.exec(`npx rimraf .env`);
+if (makeLocalEnvFile) {
+  // delete the prisma .env file
+  //console.log(`removing the prisma .env file`);
+  shell.exec(`cd ../ && npm run prisma:env:remove`);
+}
